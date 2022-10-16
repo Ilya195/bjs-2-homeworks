718df33d9bdd5c0d60ba3a233d89e2663f149675
Ilya195 <vodoley_agent@inbox.ru>
2022-10-16 15:43:44 +0600

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = false;
+  if (arr1.length == arr2.length) {
+    result = arr1.every((element, index) => {
+      return element === arr2[index]} );
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((element,int,arr) => {
+    if (element > 0 && element % 3 === 0) {
+      return element }
+    }).map(item => item*10);
   return resultArr; // array
 }
